f2a7e1d538177ab20235e11f3cbc3f971a5feb39
SteelPh0enix <wojciech_olech@hotmail.com>
2024-07-24 13:13:32 +0200

    Implemented config loading and CLI interface

---
+++ __main__.py
@@ -0,0 +1,92 @@
+"""Entry point for unreasonable-llama-discord bot"""
+
+import argparse
+import logging
+import sys
+from pathlib import Path
+
+import tomllib
+
+DEFAULT_CONFIG = """[messages]
+edit-cooldown-ms = 750
+length-limit = 1990
+remove-reaction = "💀"
+
+[commands]
+prefix = "$"
+inference-cmd = "llm"
+help-cmd = "llm-help"
+reset-conversation-cmd = "llm-reset"
+stats-cmd = "llm-stats"
+
+[bot]
+default-system-prompt = "You are a helpful AI assistant. Assist the user best to your ability."
+"""
+
+
+def load_bot_configuration(path: Path) -> dict | None:
+    if not path.exists():
+        return None
+
+    with path.open("rb") as config_file:
+        logging.info(f"Reading configuration from {path}...")
+        return tomllib.load(config_file)
+
+
+def create_default_bot_configuration(path: Path, overwrite: bool):
+    if path.exists() and not overwrite:
+        logging.critical(
+            f"Configuration file {path} exists, and i'm forbidden from overwriting it! Exiting..."
+        )
+        sys.exit(1)
+
+    with path.open("wb") as config_file:
+        config_file.write(DEFAULT_CONFIG.encode("utf-8"))
+        logging.info(f"Default config written to {path}.")
+
+
+def parse_script_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Unreasonable Llama Discord bot")
+
+    parser.add_argument(
+        "--config-file",
+        type=str,
+        default="unreasonable-config.toml",
+        help="Path to bot's configuration file. Ff the file doesn't exist, then it will be created with default configuration.",
+    )
+
+    parser.add_argument(
+        "--overwrite-config-file",
+        action="store_true",
+        default=False,
+        help="When used, config file will be overwritten if it exists but cannot be loaded. Default behavior is exitting with exit code 1 instead.",
+    )
+
+    parser.add_argument(
+        "--log-level",
+        type=str,
+        choices=["debug", "info", "warning", "error", "critical", "none"],
+        default="info",
+        help="Log level, 'info' by default",
+    )
+
+    return parser.parse_args()
+
+
+def main(args: argparse.Namespace):
+    config_path = Path(args.config_file)
+    bot_config = load_bot_configuration(config_path)
+    if bot_config is None:
+        create_default_bot_configuration(config_path, args.overwrite_config_file)
+        bot_config = load_bot_configuration(config_path)
+
+    print(f"config: {bot_config}")
+
+
+if __name__ == "__main__":
+    args = parse_script_arguments()
+    if args.log_level != "none":
+        log_level_mapping = logging.getLevelNamesMapping()
+        log_level = log_level_mapping[args.log_level.upper()]
+        logging.basicConfig(level=log_level)
+    main(args)
