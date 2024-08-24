bc4cd5ff8ec43dab70a20e42dfb54507174ba5b1
SteelPh0enix <wojciech_olech@hotmail.com>
2024-08-24 13:58:58 +0200

    Added test checking to format_and_check.ps1

--- scripts/format_and_check.ps1
+++ scripts/format_and_check.ps1
@@ -3,3 +3,9 @@ echo "Checked files: ${python_files}"
 poetry run ruff format $python_files
 poetry run mypy --strict --pretty $python_files
 poetry run ruff check --fix $python_files
+
+$python_files = Get-ChildItem -Path ./tests/*.py
+echo "Checked files: ${python_files}"
+poetry run ruff format $python_files
+poetry run mypy --strict --pretty $python_files
+poetry run ruff check --fix $python_files
