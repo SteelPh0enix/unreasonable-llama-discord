c4315e8dab9e59d15b4ca0b2e84b633588186b0f
SteelPh0enix <wojciech_olech@hotmail.com>
2024-09-02 19:57:36 +0200

    added --implicit-reexport to mypy config for tests

--- scripts/format_and_check.ps1
+++ scripts/format_and_check.ps1
@@ -7,5 +7,5 @@ poetry run ruff check --fix $python_files
 $python_files = Get-ChildItem -Path ./tests/*.py
 echo "Checked files: ${python_files}"
 poetry run ruff format $python_files
-poetry run mypy --strict --pretty $python_files
+poetry run mypy --implicit-reexport --strict --pretty $python_files
 poetry run ruff check --fix $python_files
