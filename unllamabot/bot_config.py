f6e461dac7696f2fec17609c074dc39a47b3fe07
SteelPh0enix <wojciech_olech@hotmail.com>
2024-09-25 20:15:42 +0200

    Updated bot's config

--- unllamabot/bot_config.py
+++ unllamabot/bot_config.py
@@ -15,16 +15,32 @@ edit-cooldown-ms = 750
 length-limit = 1990
 remove-reaction = "💀"
 
-[commands]
-prefix = "$"
-inference-cmd = "llm"
-help-cmd = "llm-help"
-reset-conversation-cmd = "llm-reset"
-stats-cmd = "llm-stats"
+[commands.inference]
+cmd = "llm"
+requires_admin = false
+
+[commands.help]
+cmd = "llm-help"
+requires_admin = false
+
+[commands.reset-conversation]
+cmd = "llm-reset"
+requires_admin = false
+
+[commands.stats]
+cmd = "llm-stats"
+requires_admin = false
+
+[commands.refresh]
+cmd = "llm-refresh"
+requires_admin = true
 
 [bot]
+prefix = "$"
 default-system-prompt = "You are a helpful AI assistant. Assist the user best to your ability."
 chat-database-path = "./chats.uldb"
+# add user IDs of bot administrators here, those users can call admin commands.
+admins-id = []
 
 [llama]
 # URL is optional, if not provided it will be loaded from LLAMA_CPP_SERVER_URL env variable,
@@ -34,8 +50,16 @@ server_timeout = 10000
 """
 
 
-@dataclass
-class SteelLlamaConfig:
+@dataclass(frozen=True)
+class BotCommand:
+    command: str
+    """Command string."""
+    requires_admin: bool
+    """If True, command can be executed only by user with ID from admins-id list."""
+
+
+@dataclass(frozen=True)
+class BotConfig:
     message_edit_cooldown: int
     """Time between consecutive message edits, in milliseconds."""
     message_length_limit: int
@@ -46,37 +70,40 @@ class SteelLlamaConfig:
     """Prefix character (or string) of the bot."""
     default_system_prompt: str
     """Default system prompt for the LLM."""
-    commands: dict[str, str]
-    """Mapping of Discord commands to bot's commands."""
+    admins_id: list[int]
+    """List bot administrators user IDs"""
+    commands: dict[str, BotCommand]
+    """List of commands."""
     llama_request_timeout: int
     """Timeout for llama.cpp server requests, in milliseconds."""
     llama_url: str | None
     """llama.cpp server URL, if None the LLAMA_CPP_SERVER_URL env var will be used instead."""
     chat_database_path: str
-    """Path to bot's chats and users database"""
+    """Path to bot's chats and users database."""
 
     @staticmethod
-    def from_dict(config: dict[str, Any]) -> SteelLlamaConfig:
+    def from_dict(config: dict[str, Any]) -> BotConfig:
         message_edit_cooldown = config["messages"]["edit-cooldown-ms"]
         message_length_limit = config["messages"]["length-limit"]
         message_removal_reaction = config["messages"]["remove-reaction"]
-        bot_prefix = config["commands"]["prefix"]
+        commands = {
+            command_name: BotCommand(command_info["cmd"], command_info["requires_admin"])
+            for command_name, command_info in config["commands"].items()
+        }
+        bot_prefix = config["bot"]["prefix"]
         default_system_prompt = config["bot"]["default-system-prompt"]
         chat_database_path = config["bot"]["chat-database-path"]
-        commands = {
-            command_name.removesuffix("-cmd"): command
-            for command_name, command in config["commands"].items()
-            if command_name.endswith("-cmd")
-        }
+        admin_ids = config["bot"]["admins-id"]
         llama_request_timeout = config["llama"]["server_timeout"]
         llama_url = config["llama"].get("url")
 
-        return SteelLlamaConfig(
+        return BotConfig(
             message_edit_cooldown,
             message_length_limit,
             message_removal_reaction,
             bot_prefix,
             default_system_prompt,
+            admin_ids,
             commands,
             llama_request_timeout,
             llama_url,
@@ -84,7 +111,7 @@ class SteelLlamaConfig:
         )
 
 
-def load_bot_configuration(path: Path) -> SteelLlamaConfig | None:
+def load_bot_configuration(path: Path) -> BotConfig | None:
     if not path.exists():
         return None
 
@@ -92,7 +119,7 @@ def load_bot_configuration(path: Path) -> SteelLlamaConfig | None:
         logging.info(f"Reading configuration from {path}...")
         config_json = tomllib.load(config_file)
         logging.debug(f"Read configuration JSON: {config_json}")
-        return SteelLlamaConfig.from_dict(config_json)
+        return BotConfig.from_dict(config_json)
 
 
 def create_default_bot_configuration(path: Path, overwrite: bool) -> None:
--- unllamabot/discord_client.py
+++ unllamabot/discord_client.py
@@ -7,7 +7,7 @@ import time
 from pathlib import Path
 
 import discord
-from bot_config import SteelLlamaConfig
+from bot_config import BotConfig
 from bot_database import BotDatabase, ChatRole
 from llama_backend import LlamaBackend
 from llm_utils import LLMUtils
@@ -18,7 +18,7 @@ def current_time_ms() -> int:
 
 
 class SteelLlamaDiscordClient(discord.Client):
-    def __init__(self, config: SteelLlamaConfig) -> None:
+    def __init__(self, config: BotConfig) -> None:
         self.config = config
         self.db = BotDatabase(config.chat_database_path, config.default_system_prompt)
         self.backend = LlamaBackend(
@@ -38,15 +38,14 @@ class SteelLlamaDiscordClient(discord.Client):
         model_props = self.backend.model_props()
         model_name = Path(model_props.default_generation_settings.model).name
         model_context_length = model_props.default_generation_settings.n_ctx
-        logging.debug(f"Loaded model: {model_name}")
+        logging.info(f"Loaded model: {model_name}")
 
-        await self.change_presence(
-            activity=discord.CustomActivity(
-                f"Chat with me using {self.config.bot_prefix}{self.config.commands['inference']}! "
-                f"Currently using {model_name} with context of {model_context_length} tokens per user."
-            )
+        presence_string = (
+            f"Chat with me using {self.config.bot_prefix}{self.config.commands['inference']}! "
+            + f"Currently using {model_name} with context of {model_context_length} tokens per user."
         )
-
+        logging.info(f"Bot presence: {presence_string}")
+        await self.change_presence(activity=discord.CustomActivity(presence_string))
         logging.info("Bot is ready!")
 
     async def process_inference_command(self, message: discord.Message, prompt: str | None) -> None:
@@ -96,33 +95,39 @@ class SteelLlamaDiscordClient(discord.Client):
         help_content = f"*No help available for selected subject. Try {self.config.bot_prefix}{self.config.commands['help']} for list of subjects and generic help.*"
         match subject:
             case None:
-                help_content = f"""## This is [UnreasonableLlama](https://pypi.org/project/unreasonable-llama/)-based Discord bot.
+                help_content = f"""# This is [UnreasonableLlama](https://pypi.org/project/unreasonable-llama/)-based Discord bot.
 It allows you to converse with an LLM hosted via llama.cpp.
 The bot remembers your conversations and allows you to configure the LLM in some degree.
 
-### Available commands:
-    * {self.config.bot_prefix}{self.config.commands["inference"]} [message] - chat with the LLM
-    * {self.config.bot_prefix}{self.config.commands["help"]} [subject (optional)] - show help
-    * {self.config.bot_prefix}{self.config.commands["reset-conversation"]} - clear your conversation history and start a new one
-    * {self.config.bot_prefix}{self.config.commands["stats"]} - show some stats of your conversation
-
-### Available help subjects:
-    * `model` - show model details"""
+[This bot is open-source](https://github.com/SteelPh0enix/unreasonable-llama-discord).
+## Available commands:
+    * `{self.config.bot_prefix}{self.config.commands["inference"].command} [message]` - chat with the LLM
+    * `{self.config.bot_prefix}{self.config.commands["help"].command} [subject (optional)]` - show help
+    * `{self.config.bot_prefix}{self.config.commands["reset-conversation"].command}` - clear your conversation history and start a new one
+    * `{self.config.bot_prefix}{self.config.commands["stats"].command}` - show some stats of your conversation
+## Available help subjects:
+    * `model` - show model details
+    * `admin` - show admin commands"""
             case "model":
                 props = self.backend.model_props()
                 model_info = props.default_generation_settings
-                help_content = f"""Currently loaded model: {model_info.model}
-Context length: {model_info.n_ctx}
-Samplers: {model_info.samplers}
-Top k: {model_info.top_k}
-Tfs z: {model_info.tfs_z}
-Typical p: {model_info.typical_p}
-Top p: {model_info.top_p}
-Min p: {model_info.min_p}
-Temperature: {model_info.temperature}
-Mirostat type: {model_info.mirostat}
-Mirostat Eta: {model_info.mirostat_eta}
-Mirostat Tau: {model_info.mirostat_tau}"""
+                help_content = f"""# Currently loaded model: {Path(model_info.model).name}
+**Context length**: {model_info.n_ctx} tokens
+**Samplers**: {model_info.samplers}
+## Parameters
+**Top-K**: {model_info.top_k}
+**Tail-Free Sampling Z**: {model_info.tfs_z:.02f}
+**Typical-P**: {model_info.typical_p:.02f}
+**Top-P**: {model_info.top_p:.02f}
+**Min-P**: {model_info.min_p:.02f}
+**Temperature**: {model_info.temperature:.02f}
+**Mirostat type**: {model_info.mirostat}
+**Mirostat learning rate (Eta)**: {model_info.mirostat_eta:.02f}
+**Mirostat target entropy (Tau)**: {model_info.mirostat_tau:.02f}"""
+            case "admin":
+                help_content = f"""# Admin commands
+* `{self.config.bot_prefix}{self.config.commands["refresh"].command}` - refresh llama.cpp props"""
+
         await message.reply(content=help_content)
 
     async def process_reset_conversation_command(self, message: discord.Message) -> None:
@@ -165,13 +170,13 @@ Current prompt length (characters): {prompt_length_chars}"""
             f"<UID:{message.author.id}|UN:{message.author.global_name}> Command detected: {command_name}, arguments: {arguments}"
         )
 
-        if command_name == self.config.commands["inference"]:
+        if command_name == self.config.commands["inference"].command:
             await self.process_inference_command(message, arguments)
-        elif command_name == self.config.commands["help"]:
+        elif command_name == self.config.commands["help"].command:
             await self.process_help_command(message, arguments)
-        elif command_name == self.config.commands["reset-conversation"]:
+        elif command_name == self.config.commands["reset-conversation"].command:
             await self.process_reset_conversation_command(message)
-        elif command_name == self.config.commands["stats"]:
+        elif command_name == self.config.commands["stats"].command:
             await self.process_stats_command(message)
         else:
             await message.reply(f"Unknown command: {command_name}")
